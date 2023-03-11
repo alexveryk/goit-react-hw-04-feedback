1b1b20177a862ed482c4bba223096ebf3144f141
alexveryk <veryk.av@gmail.com>
2023-03-11 23:39:13 +0200

    Create FeedbackOptions.styled.jsx

---
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const FeedbackBtn = styled.button`
+  font-size: 14px;
+  font-weight: 600;
+  padding: 5px 15px;
+  margin-right: 10px;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  box-shadow: 2px 2px 2px 2px #cabdbd;
+`;
+
+export default FeedbackBtn;
